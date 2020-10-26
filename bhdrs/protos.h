3aeda0ce8e4aff3e481e8c5a490add0eb8a77aff
Craig Spannring <craig.spannring@montanainstruments.com>
2020-10-26 17:10:44 -0600

    adding function prototypes

---
+++ bhdrs/protos.h
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * 
+ */    
+#ifndef PROTOS_h_9ddc048ebe87687cdacca640cbfb6710
+#define PROTOS_h_9ddc048ebe87687cdacca640cbfb6710
+
+#include "b.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    
+    Visible bool nothing(txptr q; int m);
+
+
+
+#ifdef __cplusplus
+extern "C" }
+#endif
+
+#endif
