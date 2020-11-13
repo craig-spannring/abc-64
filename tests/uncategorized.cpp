52f6e606a40cbff1213f10602129fffe33bbc3e6
Craig Spannring <craig.spannring@montanainstruments.com>
2020-11-12 21:21:25 -0700

    Add some unit tests.

--- tests/uncategorized.cpp
+++ tests/uncategorized.cpp
@@ -4,10 +4,37 @@
 
 #include "gtest/gtest.h"
 
-TEST(whitebox, 
+#include "b.h"
+#include "bobj.h"
+
+extern "C"
 {
-	ASSERT_DOUBLE_EQ(1.0, 1.00000000000000);
+	Visible char *OPTbwsdir= (char *) NULL;
+	Visible char *OPTworkspace= (char *) NULL;
+	Visible char *OPTcentral= (char *) NULL;
+	Visible char *OPTeditor= (char *) NULL;
+	Visible bool OPTunpack= No;
+	Visible bool OPTslowterminal= No;
+	Visible int abc_todo = 1;
 }
+
+
+TEST(whitebox, mk_integer_001)
+{
+	integer  myInt;
+	
+	value y = mk_integer(1);
+	EXPECT_TRUE(IsSmallInt(y));
+
+	EXPECT_EQ(mk_integer(1),  reinterpret_cast<void*>(3)); // funky pointer stuff for small integer values
+ 	EXPECT_EQ(reinterpret_cast<intptr_t>(mk_integer(-1)),
+						static_cast<intptr_t>(-1)); 
+}
+	
+TEST(whitebox, mk_integer_002)
+{
+}
+
 
 int main(int argc, char** argv)
 {
